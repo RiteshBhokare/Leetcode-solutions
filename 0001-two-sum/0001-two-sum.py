d3e398b7cf20b809a353e02ebd93fde3e6917287
Ritesh Bhokare <160627330+RiteshBhokare@users.noreply.github.com>
2025-09-24 10:58:35 +0530

    Time: 2302 ms (11.69%), Space: 13.2 MB (47.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,11 +6,16 @@ class Solution(object):
         :rtype: List[int]
         """
         l1=[]
+        flag = False
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if(nums[i] + nums[j] == target):
                     l1.append(i)
                     l1.append(j)
+                    flag = True
+                if flag:
                     break
+            if flag:
+                break
         return l1
                     

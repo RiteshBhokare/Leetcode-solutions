59fd22069a6be7326ca82b796e078088e18f869a
Ritesh Bhokare <160627330+RiteshBhokare@users.noreply.github.com>
2025-09-30 23:34:12 +0530

    Time: 1 ms (42.35%), Space: 12.5 MB (27.39%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,12 +1,15 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
+        '''sorting list in lexicographical order'''
         strs.sort()
-        str1 = ""
+        prefix = ""
         for i in range(len(strs[0])):
-            le = strs[0]
-            ri = strs[-1]
-            if (le[i] == ri[i]):
-                str1 += le[i]
-            else:
-                break
-        return str1
+            '''bcoz of sorting, prefix of first ele and last ele will 
+            be same for whole list'''
+            if (strs[0][i] != strs[-1][i]):
+                return prefix
+            else: 
+                prefix += strs[0][i]
+        return prefix
+                
+        

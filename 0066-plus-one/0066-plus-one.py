59883df266ca557ec48802b59b27484fce73a634
Ritesh Bhokare <160627330+RiteshBhokare@users.noreply.github.com>
2025-10-03 23:54:23 +0530

    Time: 0 ms (100%), Space: 12.4 MB (80.72%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,11 +1,14 @@
 class Solution(object):
     def plusOne(self, digits):
+        #to convert the list into a single string
         num = ""
         for dig in digits:
             num += str(dig)
-        numStr = str(int(num) + 1)
+        #converting string into integer and then adding 1 and again converting into string to iterate on it
+        newNumStr = str(int(num) + 1)
         ret =[]
-        for i in numStr:
+        for i in newNumStr:
+            #converting into integer and storing into list
             ret.append(int(i))
      
         return ret

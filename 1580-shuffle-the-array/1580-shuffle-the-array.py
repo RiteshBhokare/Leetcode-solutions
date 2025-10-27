f822b10d834a77fbb3fbcdc66eb3f6edaeec9be9
Ritesh Bhokare <160627330+RiteshBhokare@users.noreply.github.com>
2025-10-27 16:47:27 +0530

    Time: 65 ms (14.18%), Space: 17.9 MB (86.9%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -1,9 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
+        l1 = nums[:n]
+        l2 = nums[n:]
         l3=[]
         for i in range(n):
-            l3.append(nums[i])
-            l3.append(nums[i+n])
+            l3.extend([l1[i], l2[i]])
+            
         return l3
 
   

d875e089af53b79f338cad49666f6562888e6ae6
Ritesh Bhokare <160627330+RiteshBhokare@users.noreply.github.com>
2025-11-03 22:41:56 +0530

    Time: 32 ms (87.64%), Space: 17.6 MB (64.48%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,10 +1,5 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
 class Solution:
     def interpret(self, command: str) -> str:
-        res = ""
-        command = command.replace("()", "o")
-        for ele in command:
-            if (ele != "(") & (ele != ")"):
-                res += ele
-        return res    
+        return command.replace("()", "o").replace("(al)", "al")
 

0f8edfff26aec9ae8c5ef2bf2fcd505559543777
Ritesh Bhokare <160627330+RiteshBhokare@users.noreply.github.com>
2026-01-27 23:58:59 +0530

    Time: 338 ms (14.91%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -4,6 +4,6 @@ from (select requester_id id
         from RequestAccepted
         union all
         select accepter_id id from RequestAccepted) b
-group by 1
-order by 2 desc
-limit 1 
+group by id
+order by num desc
+limit 1
